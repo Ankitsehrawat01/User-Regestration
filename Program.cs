8fd32faade36d625362622cceca463694bf47a53
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-22 20:35:28 +0530

    enter valid firstname

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace User_Registration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            User.ValidFirstName();
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User_Registration
+{
+    public class User
+    {
+        public static void ValidFirstName()
+        {
+            Console.WriteLine("Enter First Name");
+            string data = Console.ReadLine();
+            string namepattern = "^[A-Z]{1}[A-Za-z]{3,}";
+            Regex regexobj = new Regex(namepattern);
+            if(regexobj.IsMatch(data))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Name is valid");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Name is not valid");
+                Console.ResetColor();
+            }
+            Console.WriteLine(data);
+        }
+    }
+}
